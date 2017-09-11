724531dbbf3d918b7bf17800d4e7e4582e5bd947
Juha Kiili <juha@juhakiili.com>
2017-09-11 12:13:03 +0300

    Move tile preview code to separate file

--- Assets/Tilemap/Rule Tiles/Scripts/Editor/RuleTileEditor.cs
+++ Assets/Tilemap/Rule Tiles/Scripts/Editor/RuleTileEditor.cs
@@ -292,52 +292,9 @@ namespace UnityEditor
 		public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
 		{
 			if (tile.m_DefaultSprite != null)
-			{
-				Type t = GetType("UnityEditor.SpriteUtility");
-				if (t != null)
-				{
-					MethodInfo method = t.GetMethod("RenderStaticPreview", new Type[] {typeof (Sprite), typeof (Color), typeof (int), typeof (int)});
-					if (method != null)
-					{
-						object ret = method.Invoke("RenderStaticPreview", new object[] {tile.m_DefaultSprite, Color.white, width, height});
-						if (ret is Texture2D)
-							return ret as Texture2D;
-					}
-				}
-			}
+				return PreviewUtil.RenderStaticPreview(tile.m_DefaultSprite, width, height);
+			
 			return base.RenderStaticPreview(assetPath, subAssets, width, height);
-		}
-
-		private static Type GetType(string TypeName)
-		{
-			var type = Type.GetType(TypeName);
-			if (type != null)
-				return type;
-
-			if (TypeName.Contains("."))
-			{
-				var assemblyName = TypeName.Substring(0, TypeName.IndexOf('.'));
-				var assembly = Assembly.Load(assemblyName);
-				if (assembly == null)
-					return null;
-				type = assembly.GetType(TypeName);
-				if (type != null)
-					return type;
-			}
-
-			var currentAssembly = Assembly.GetExecutingAssembly();
-			var referencedAssemblies = currentAssembly.GetReferencedAssemblies();
-			foreach (var assemblyName in referencedAssemblies)
-			{
-				var assembly = Assembly.Load(assemblyName);
-				if (assembly != null)
-				{
-					type = assembly.GetType(TypeName);
-					if (type != null)
-						return type;
-				}
-			}
-			return null;
 		}
 
 		private static Texture2D Base64ToTexture(string base64)
