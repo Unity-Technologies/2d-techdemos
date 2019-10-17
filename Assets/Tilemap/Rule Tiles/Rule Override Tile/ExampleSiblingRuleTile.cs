4a33eb19475c46b03030d91e7dc1ef97c7e8c958
ChuanXin-Unity <chuanxin@unity3d.com>
2019-10-17 17:47:36 +0800

    -Update Rule Override Tile example (Thanks to johnsoncodehk!)

---
+++ Assets/Tilemap/Rule Tiles/Rule Override Tile/ExampleSiblingRuleTile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class ExampleSiblingRuleTile : RuleTile
+{
+
+    public enum SibingGroup
+    {
+        Poles,
+        Terrain,
+    }
+    public SibingGroup sibingGroup;
+
+    public override bool RuleMatch(int neighbor, TileBase other)
+    {
+        if (other is RuleOverrideTile)
+            other = (other as RuleOverrideTile).m_InstanceTile;
+
+        switch (neighbor)
+        {
+            case TilingRule.Neighbor.This:
+                {
+                    return other is ExampleSiblingRuleTile
+                        && (other as ExampleSiblingRuleTile).sibingGroup == this.sibingGroup;
+                }
+        }
+        return base.RuleMatch(neighbor, other);
+    }
+}
