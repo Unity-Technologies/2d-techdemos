942525f4324528fda36614ee7097bd10d94e7711
ChuanXin-Unity <chuanxin@unity3d.com>
2019-11-14 18:08:36 +0800

    -Add CustomAxisSortCamera

---
+++ Assets/Tilemap/IsometricZAsY/Scripts/CustomAxisSortCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomAxisSortCamera : MonoBehaviour 
+{
+    void Start()
+    {
+        var camera = GetComponent<Camera>();
+        camera.transparencySortMode = TransparencySortMode.CustomAxis;
+        camera.transparencySortAxis = new Vector3(0.0f, 0.5f, -0.25f);
+    }
+}
